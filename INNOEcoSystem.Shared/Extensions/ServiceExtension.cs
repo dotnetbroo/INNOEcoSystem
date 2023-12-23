fbb45b2ce4e75e73eb50b2b63c02ebe70109c1e6
dotnetbroo <jasurbekabdunazarov04@gmail.com>
2023-12-23 17:09:43 +0500

    Updateed

--- INNOEcoSystem.Shared/Extensions/ServiceExtension.cs
+++ INNOEcoSystem.Shared/Extensions/ServiceExtension.cs
@@ -1,30 +1,27 @@
 using INNOEcoSystem.Data.IRepositories;
 using INNOEcoSystem.Data.IRepositories.Categories;
+using INNOEcoSystem.Data.IRepositories.Depsrtments;
+using INNOEcoSystem.Data.IRepositories.Locations;
 using INNOEcoSystem.Data.IRepositories.Users;
 using INNOEcoSystem.Data.Repositories;
 using INNOEcoSystem.Data.Repositories.Categories;
+using INNOEcoSystem.Data.Repositories.Departments;
+using INNOEcoSystem.Data.Repositories.Locations;
 using INNOEcoSystem.Data.Repositories.Users;
 using INNOEcoSystem.Service.Interfaces.Accaunts;
+using INNOEcoSystem.Service.Interfaces.Applications;
+using INNOEcoSystem.Service.Interfaces.Department;
 using INNOEcoSystem.Service.Interfaces.Departments;
+using INNOEcoSystem.Service.Interfaces.Location;
 using INNOEcoSystem.Service.Interfaces.Locations;
 using INNOEcoSystem.Service.Interfaces.User;
+using INNOEcoSystem.Service.Services;
 using INNOEcoSystem.Service.Services.Accaunts;
+using INNOEcoSystem.Service.Services.Applications;
 using INNOEcoSystem.Service.Services.Departments;
 using INNOEcoSystem.Service.Services.Locations;
-
-using INNOEcoSystem.Data.Repositories.Locations;
-using INNOEcoSystem.Domain.Entities.Locations;
-
-using INNOEcoSystem.Data.Repositories;
-using INNOEcoSystem.Service.Interfaces.Applications;
-using INNOEcoSystem.Service.Interfaces.User;
-using INNOEcoSystem.Service.Services.Applications;
 using INNOEcoSystem.Service.Services.Users;
 using Microsoft.Extensions.DependencyInjection;
-using INNOEcoSystem.Service.Interfaces.Department;
-using INNOEcoSystem.Service.Services;
-using INNOEcoSystem.Data.IRepositories.Depsrtments;
-using INNOEcoSystem.Data.Repositories.Departments;
 
 namespace INNOEcoSystem.Shared.Extensions;
 
@@ -36,14 +33,15 @@ public static class ServiceExtensions
         services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         services.AddScoped<IAddressRepository, AddressRepository>();
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IApplicationRepository, ApplicationRepository>();
 
         // Category
         services.AddScoped<ICategoryService, CategoryService>();
         services.AddScoped<ICategoryRepository, CategoryRepository>();
 
         //Department 
-        services.AddScoped<IDepartmentService,DepartmentService>();
-        services.AddScoped<IDepartmentRepository,DepartmentRepository>();
+        services.AddScoped<IDepartmentService, DepartmentService>();
+        services.AddScoped<IDepartmentRepository, DepartmentRepository>();
         // Location
         services.AddScoped<ILocationService, LocationService>();
         services.AddScoped<ILocationRepository, LocationRepository>();
