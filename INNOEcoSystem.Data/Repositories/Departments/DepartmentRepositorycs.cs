b9937fe49e330556658ca191a96309026f03b8b3
Dilshodov01 <dilshodovziyodillo@gmail.com>
2023-12-23 10:45:38 +0500

    Department Repository added

---
+++ INNOEcoSystem.Data/Repositories/Departments/DepartmentRepositorycs.cs
@@ -0,0 +1,12 @@
+﻿using INNOEcoSystem.Data.DbContexts;
+using INNOEcoSystem.Domain.Entities.Departments;
+using INNOEcoSystem.Data.IRepositories.Depsrtments;
+
+namespace INNOEcoSystem.Data.Repositories.Departments;
+
+public class DepartmentRepositorycs : Repository<Department>, IDepartmentRepository
+{
+    public DepartmentRepositorycs(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+}
