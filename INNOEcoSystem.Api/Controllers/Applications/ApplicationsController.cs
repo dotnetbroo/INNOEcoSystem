32c458098b45a2900e4f377f0f657d9df1eb4a9a
bekoo1995 <oybekuzganboev@gmail.com>
2023-12-24 11:34:26 +0500

    resolved conflicts

---
+++ INNOEcoSystem.Api/Controllers/Applications/ApplicationsController.cs
@@ -0,0 +1,37 @@
+﻿using INNOEcoSystem.Api.Controllers.Commons;
+using INNOEcoSystem.Domain.Configurations;
+using INNOEcoSystem.Service.DTOs.Applications;
+using INNOEcoSystem.Service.Interfaces.Applications;
+using Microsoft.AspNetCore.Mvc;
+
+namespace INNOEcoSystem.Api.Controllers.Applications;
+
+public class ApplicationsController : BaseController
+{
+    private readonly IApplicationService _applicationService;
+
+    public ApplicationsController(IApplicationService applicationService)
+    {
+        _applicationService = applicationService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> InsertAsync([FromBody] ApplicationForCreationDto dto)
+        => Ok(await _applicationService.AddAsync(dto));
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+        => Ok(await _applicationService.RetrieveAllAsync(@params));
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute] long id)
+        => Ok(await _applicationService.RetrieveByIdAsync(id));
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> RemoveAsync([FromRoute] long id)
+        => Ok(await _applicationService.RemoveAsync(id));
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateAsync([FromRoute] short id, [FromBody] ApplicationForUpdateDto dto)
+        => Ok(await _applicationService.ModifyAsync(id, dto));
+}
