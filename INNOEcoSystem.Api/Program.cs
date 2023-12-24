b0d9c6822c9d0563ef3d321878921f907562a4f6
bekoo1995 <oybekuzganboev@gmail.com>
2023-12-24 13:48:14 +0500

    some-shanges

--- INNOEcoSystem.Api/Program.cs
+++ INNOEcoSystem.Api/Program.cs
@@ -65,7 +65,7 @@ WebHostEnviromentHelper.WebRootPath = Path.GetFullPath("wwwroot");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
